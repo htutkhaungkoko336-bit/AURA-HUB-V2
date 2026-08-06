e721fe1946005447eced7b549760ea2a7ffbd68b
BarboSSA <htutkhaungkoko336@gmail.com>
2026-08-06 21:33:15 +0630

    509

--- script.js
+++ script.js
@@ -603,7 +603,6 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 let isWheelOpen = false;
 
-let isWheelOpen = false;
 
 window.toggleActionWheel = function() {
     isWheelOpen = !isWheelOpen;
